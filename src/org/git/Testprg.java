6c150f922a781af03446aedcf5993f9adbe6114d
arjun <arjunselvan7@gmail.com>
2021-05-29 18:25:23 +0530

    code added

---
+++ src/org/git/Testprg.java
@@ -0,0 +1,13 @@
+package org.git;
+
+public class Testprg {
+
+	public static void print(String str) {
+		System.out.println("This " +str+ " is my first git program...");
+
+	}
+	public static void main(String[] args) {
+		print("test112");
+	}
+}
+
